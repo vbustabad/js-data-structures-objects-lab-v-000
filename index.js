33c536a6ead11b480c460dc011a2d37a88aca02a
Veronica Bustabad <vbustabad@gmail.com>
2018-09-05 02:30:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const new_driver = { ...driver };
+
+  new_driver[key] = value;
+
+  return new_driver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+
+  return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+  const newdriver = { ...driver };
+  delete newdriver.key;
+  return newdriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver.key;
+}
